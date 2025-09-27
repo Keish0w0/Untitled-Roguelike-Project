86ba0e3f41ce98f231cc4fe43afa202105d5bac2
Keish0w0 <potatokeish0w0@gmail.com>
2025-09-27 23:00:26 +0800

    InDev 20250927-5

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,18 +1,18 @@
 class_name Player extends CharacterBody2D
 
 @export_subgroup("Player Movement")
-@export var max_speed = 650
-@export var accel = 6
-@export var friction = 8
-@export var dash_speed = max_speed * 3.5
+@export var max_speed : float  = 650
+@export var accel : float  = 6
+@export var friction : float  = 8
+@export var dash_speed : float = max_speed * 3.5
 
 ##DASH
-@onready var dash_stamina = 25
-var can_dash = true
-var is_dashing = false
+@onready var dash_stamina : float = 25
+var can_dash : bool = true
+var is_dashing : bool = false
 
 ##DAMAGE
-var dmg_cooldown = false
+var dmg_cooldown : bool = false
 
 ##PLAYER INPUT
 var input = Vector2.ZERO
