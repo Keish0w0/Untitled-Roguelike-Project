0c066ac4e74e2c6788132db094d12cd46f7bc90c
Keish0w0 <potatokeish0w0@gmail.com>
2025-10-07 21:05:27 +0800

    InDev 20251007-1

--- Scripts/Enemy/basic_enemy.gd
+++ Scripts/Enemy/basic_enemy.gd
@@ -2,8 +2,9 @@ extends CharacterBody2D
 
 @export var player_ref: Player
 var max_speed: float
-var direction: Vector2
 var damage: float
+var knockback: Vector2
+var seperation: float
 
 var type: Enemy:
 	set(value):
@@ -11,15 +12,31 @@ var type: Enemy:
 		$AnimationComponent/Sprite2D.texture = value.texture
 		damage = value.damage
 		max_speed = value.speed
-	
 
 func _physics_process(delta: float) -> void:
-	velocity = velocity.lerp((player_ref.position - position).normalized() * max_speed, 1)
-	move_and_collide(velocity * delta)
+	check_seperation(delta)
+	knockback_update(delta)
+	
+	#ANIMATION
 	flip_sprite()
 	$AnimationComponent.run()
 
+func check_seperation(_delta):
+	seperation = (player_ref.position - position).length()
+	if seperation >= 1000:
+		queue_free()
+
+func knockback_update(delta):
+	#MOVEMENT
+	#velocity = velocity.lerp((player_ref.position - position).normalized() * max_speed, 1)
+	velocity = (player_ref.position - position).normalized() * max_speed
+	knockback = knockback.move_toward(Vector2.ZERO, 1)
+	velocity += knockback
+	var collider = move_and_collide(velocity * delta)
+	if collider:
+		collider.get_collider().knockback = (collider.get_collider().global_position - global_position).normalized() * 35
+
 func flip_sprite():
-	$AnimationComponent/Sprite2D.flip_h = velocity.x < 0
+	$AnimationComponent/Sprite2D.flip_h = global_position.direction_to(player_ref.global_position).x < 0
 
 func contact_damage(): pass
--- Scripts/spawner.gd
+++ Scripts/spawner.gd
@@ -8,18 +8,13 @@ extends Node2D
 
 @export var enemy_types: Array[Enemy]
 
-var minute: int:
-	set(value):
-		minute = value
-		%Minute.text = str(value)
-
+var minute: int
 var second: int:
 	set(value):
 		second = value
-		if second >= 59:
-			second -= 59
+		if second >= 60:
+			second -= 60
 			minute += 1
-		%Second.text = str(value).lpad(2, '0')
 
 func spawn(pos: Vector2):
 	var enemy_instance = enemy.instantiate()
@@ -39,8 +34,10 @@ func amount(number: int = 1):
 
 func _on_timer_timeout() -> void:
 	second += 1
-	amount(second % 10)
+	%Second.text = str(second).lpad(2, '0')
+	%Minute.text = str(minute).lpad(2, '0')
+	amount((minute % 10)+1) 
 
 func _on_pattern_timeout() -> void:
-	for i in range(85):
+	for i in range(50):
 		spawn(get_random_pos())
