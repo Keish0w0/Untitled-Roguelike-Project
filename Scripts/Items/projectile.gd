4810875f6077b5f6d28333488131b74edee8cbd4
Keish0w0 <potatokeish0w0@gmail.com>
2025-11-06 17:49:05 +0800

    InDev 20251106-0

--- Scripts/Items/projectile.gd
+++ Scripts/Items/projectile.gd
@@ -6,12 +6,18 @@ var direction : Vector2 = Vector2.RIGHT
 var speed : float = 200
 var damage : float = 1
 var type : String
+var pierce_count : float = 0
+var pierce_limit : float = 1
 
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 
 func _on_body_entered(body: Node2D) -> void:
-	#queue_free()
+	pierce_count += 1
+	
+	if pierce_count >= pierce_limit:
+		queue_free()
+	
 	if body.has_node("EnemyHitboxComponent"):
 		body.hitbox.take_damage(damage)
 		body.knockback = direction * 50
